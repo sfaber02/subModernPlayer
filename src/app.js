ea6596136d784d5f37126f5c330d5ba930bd4752
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-03-27 19:40:03 -0400

    more grid

--- src/app.js
+++ src/app.js
@@ -36,6 +36,104 @@ const App = () => {
             notes: ['first bounce with vocals, rough covid demo'],
             date: '01/09/22',
             src: './audio-files/03 - Lightning Bolt - 13 Monsters.mp3'
+        },
+        {
+            id: 0,
+            title: 'Rubber Lips',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/01 Damaged Bug – Rubber Lips.mp3',
+        },
+        {
+            id: 1,
+            title: 'Game of Pricks',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/07 - Game Of Pricks.mp3',
+        },
+        {
+            id: 2,
+            title: 'Vitamin C',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/VitaminC.mp3'
+        },
+        {
+            id: 3,
+            title: '13 Monsters',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/03 - Lightning Bolt - 13 Monsters.mp3'
+        },
+        {
+            id: 0,
+            title: 'Rubber Lips',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/01 Damaged Bug – Rubber Lips.mp3',
+        },
+        {
+            id: 1,
+            title: 'Game of Pricks',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/07 - Game Of Pricks.mp3',
+        },
+        {
+            id: 2,
+            title: 'Vitamin C',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/VitaminC.mp3'
+        },
+        {
+            id: 3,
+            title: '13 Monsters',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/03 - Lightning Bolt - 13 Monsters.mp3'
+        },
+        {
+            id: 2,
+            title: 'Vitamin C',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/VitaminC.mp3'
+        },
+        {
+            id: 3,
+            title: '13 Monsters',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/03 - Lightning Bolt - 13 Monsters.mp3'
+        },
+        {
+            id: 0,
+            title: 'Rubber Lips',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/01 Damaged Bug – Rubber Lips.mp3',
+        },
+        {
+            id: 1,
+            title: 'Game of Pricks',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/07 - Game Of Pricks.mp3',
+        },
+        {
+            id: 2,
+            title: 'Vitamin C',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/VitaminC.mp3'
+        },
+        {
+            id: 3,
+            title: '13 Monsters',
+            notes: ['first bounce with vocals, rough covid demo'],
+            date: '01/09/22',
+            src: './audio-files/03 - Lightning Bolt - 13 Monsters.mp3'
         }
     ]
 
