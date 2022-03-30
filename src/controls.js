b011a5e7e74a403719889deb2dbc352b88085388
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-03-30 19:53:02 -0400

    flipped play and pause buttons

--- src/controls.js
+++ src/controls.js
@@ -2,13 +2,12 @@ import React from 'react';
 import buttons from "./graphics/buttons.js"
 
 const Controls = (props) => {
-    console.log (buttons);
     
     return (
         <div id='SMPtransportButtons'>
             <button className='SMPcontrolButtons' onClick={props.prev}><img className='SMPcontrolButtonsImg' src={buttons.prev} /><img className='SMPcontrolButtonsImg' src={buttons.prev} /></button>
-            <button className='SMPcontrolButtons' onClick={props.play}><img className='SMPcontrolButtonsImg' src={buttons.bigPlay} /></button>
             <button className='SMPcontrolButtons' onClick={props.pause}><img className='SMPcontrolButtonsImg' src={buttons.pause} /></button>
+            <button className='SMPcontrolButtons' onClick={props.play}><img className='SMPcontrolButtonsImg' src={buttons.bigPlay} /></button>
             <button className='SMPcontrolButtons' onClick={props.stop}><img className='SMPcontrolButtonsImg' src={buttons.stop} /></button>
             <button className='SMPcontrolButtons' onClick={props.next}><img className='SMPcontrolButtonsImg' src={buttons.bigPlay} /><img className='SMPcontrolButtonsImg' src={buttons.bigPlay} /></button>
         </div>
